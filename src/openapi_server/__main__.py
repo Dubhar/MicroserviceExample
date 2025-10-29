bdf37298e9db1d2c82130b786f1ffeb60f7722a9
Thorsten Jerosch <t.jerosch@kungf.ooo>
2025-10-29 20:53:20 +0100

    rm flask example

--- src/openapi_server/__main__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-from openapi_server.app import connexion_app
-
-# For local debugging only, Dockerfile uses app.py
-if __name__ == "__main__":
-    connexion_app.run(port=8080, host="0.0.0.0", debug=False)
-
--- src/openapi_server/models/__init__.py
+++
@@ -1,3 +0,0 @@
-# flake8: noqa
-# import models into model package
-from openapi_server.models.cv_get404_response import CvGet404Response
